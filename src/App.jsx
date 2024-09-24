c2e2d7f7eb14658e61799ff6dfdbea98b702c1aa
Royal-attryb <royal@attryb.com>
2024-09-24 19:44:09 +0530

    added login button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 function App() {
     return (
         <div className="App">
-            Hello
+            <a 
+                href="https://www.facebook.com/dialog/oauth?client_id=1030096775084808&display=page&extras={'setup':{'channel':'IG_API_ONBOARDING'}}&redirect_uri=https://successful-login-page.vercel.app/&response_type=token&scope=instagram_basic,pages_read_engagement,pages_show_list,business_management"
+                target="blank"
+            >
+                Login
+            </a>
         </div>
     );
 }
